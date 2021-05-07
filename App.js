58ccee6a81ece17313603f4a346ce249c8905176
crs <ornaldo.hasa@fshnstudent.info>
2021-05-07 16:01:21 +0200

    m

---
+++ App.js
@@ -0,0 +1,14 @@
+import TimeInput from "./lib/index"
+import React,{useState} from "react";
+
+function App() {
+  const [date,setDate]=useState("12:00")
+  return (
+    <TimeInput hour12Format value={date} onChange={(dateString)=>{
+       console.log(dateString)
+       setDate(date)
+     }}/>
+  );
+}
+
+export default App;
