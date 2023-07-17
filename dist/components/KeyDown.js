2e95a0663059733ac6c4f680774ee270437a64dc
crs <ornaldo.hasa@fshnstudent.info>
2023-07-17 17:15:01 +0200

    m

--- dist/components/KeyDown.js
+++ dist/components/KeyDown.js
@@ -1,71 +1,43 @@
-import React, { useRef, useRef } from "react";
-
+import React, { useEffect, useRef } from "react";
 var keydownInterruptedRef = false;
-
-class KeyDown extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onKeyDownRef = useRef(props.onKeyDown);
-    this.reference = props.reference;
-  }
-
-  componentDidMount() {
-    this.onKeyDownRef.current = this.props.onKeyDown;
-
-    var element = this.reference && this.reference.current;
-    var handleKeyDown = (event) => {
+var KeyDown = props => {
+  var onKeyDown = props.onKeyDown,
+    children = props.children,
+    reference = props.reference;
+  var onKeyDownRef = useRef(onKeyDown);
+  useEffect(() => {
+    onKeyDownRef.current = onKeyDown;
+  }, [onKeyDown]);
+  useEffect(() => {
+    onKeyDownRef.current = onKeyDown;
+    var element = reference === null || reference === void 0 ? void 0 : reference.current;
+    var handleKeyDown = event => {
       var _onKeyDownRef$current;
       keydownInterruptedRef = true;
-      if (this.onKeyDownRef && this.onKeyDownRef.current) {
-        (_onKeyDownRef$current = this.onKeyDownRef.current).call(this.onKeyDownRef, event);
-      }
+      onKeyDownRef === null || onKeyDownRef === void 0 || (_onKeyDownRef$current = onKeyDownRef.current) === null || _onKeyDownRef$current === void 0 ? void 0 : _onKeyDownRef$current.call(onKeyDownRef, event);
     };
-    var handleKeyUp = (event) => {
+    var handleKeyUp = event => {
       var _onKeyDownRef$current2;
-      if (!keydownInterruptedRef && this.onKeyDownRef && this.onKeyDownRef.current) {
-        (_onKeyDownRef$current2 = this.onKeyDownRef.current).call(this.onKeyDownRef, event);
-      }
+      if (!keydownInterruptedRef) onKeyDownRef === null || onKeyDownRef === void 0 || (_onKeyDownRef$current2 = onKeyDownRef.current) === null || _onKeyDownRef$current2 === void 0 ? void 0 : _onKeyDownRef$current2.call(onKeyDownRef, event);
       keydownInterruptedRef = false;
     };
     if (element) {
       element.addEventListener("keydown", handleKeyDown, {
-        capture: true,
+        capture: true
       });
       element.addEventListener("keyup", handleKeyUp, {
-        capture: true,
+        capture: true
       });
+      return () => {
+        element.removeEventListener("keydown", handleKeyDown, {
+          capture: true
+        });
+        element.removeEventListener("keyup", handleKeyUp, {
+          capture: true
+        });
+      };
     }
-  }
-
-  componentWillUnmount() {
-    var element = this.reference && this.reference.current;
-    var handleKeyDown = (event) => {
-      var _onKeyDownRef$current;
-      keydownInterruptedRef = true;
-      if (this.onKeyDownRef && this.onKeyDownRef.current) {
-        (_onKeyDownRef$current = this.onKeyDownRef.current).call(this.onKeyDownRef, event);
-      }
-    };
-    var handleKeyUp = (event) => {
-      var _onKeyDownRef$current2;
-      if (!keydownInterruptedRef && this.onKeyDownRef && this.onKeyDownRef.current) {
-        (_onKeyDownRef$current2 = this.onKeyDownRef.current).call(this.onKeyDownRef, event);
-      }
-      keydownInterruptedRef = false;
-    };
-    if (element) {
-      element.removeEventListener("keydown", handleKeyDown, {
-        capture: true,
-      });
-      element.removeEventListener("keyup", handleKeyUp, {
-        capture: true,
-      });
-    }
-  }
-
-  render() {
-    return this.props.children;
-  }
-}
-
+  }, []);
+  return children;
+};
 export default KeyDown;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/lib/index.js", // Entry point of your package
+  output: {
+    filename: "index.js",
+    path: __dirname + "/dist",
+    libraryTarget: "umd",
+    library: "YourPackageName",
+    umdNamedDefine: true,
+    globalObject: "typeof self !== 'undefined' ? self : this",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    chrome: "49", // Set Chrome 49 as the target
+                  },
+                  useBuiltIns: "usage",
+                  corejs: "3.20.3",
+                },
+              ],
+              "@babel/preset-react",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  externals: {
+    react: "React", // Exclude React from the bundle if it's a peer dependency
+    "react-dom": "ReactDOM", // Exclude ReactDOM if it's a peer dependency
+  },
+};
