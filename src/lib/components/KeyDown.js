bfeb6bd66ea8be185a955c6457420ec754b9cb5e
crs <ornaldo.hasa@fshnstudent.info>
2023-07-17 18:36:48 +0200

    m

--- src/lib/components/KeyDown.js
+++ src/lib/components/KeyDown.js
@@ -20,19 +20,34 @@ const KeyDown = (props) => {
 
     const handleKeyUp = (event) => {
       if (!keydownInterruptedRef) onKeyDownRef?.current?.(event);
-       keydownInterruptedRef = false;
+      keydownInterruptedRef = false;
     };
 
-    if (element){
-      element.addEventListener("keydown", handleKeyDown, { capture: true });
-      element.addEventListener("keyup", handleKeyUp, { capture: true });
+    if (element) {
+      if (isChrome51OrLower()) {
+        element.addEventListener("keydown", handleKeyDown);
+        element.addEventListener("keyup", handleKeyUp);
+      } else {
+        element.addEventListener("keydown", handleKeyDown, { capture: true });
+        element.addEventListener("keyup", handleKeyUp, { capture: true });
+      }
 
       return () => {
-        element.removeEventListener("keydown", handleKeyDown, { capture: true });
-        element.removeEventListener("keyup", handleKeyUp, { capture: true });
+        if (isChrome51OrLower()) {
+          element.removeEventListener("keydown", handleKeyDown);
+          element.removeEventListener("keyup", handleKeyUp);
+        } else {
+          element.removeEventListener("keydown", handleKeyDown, { capture: true });
+          element.removeEventListener("keyup", handleKeyUp, { capture: true });
+        }
       };
-    } 
+    }
   }, []);
+
+  const isChrome51OrLower = () => {
+    const chromeVersion = parseInt(navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./)[2]);
+    return chromeVersion <= 51;
+  };
 
   return children;
 };
